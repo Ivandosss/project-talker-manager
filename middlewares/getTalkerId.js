f47d49ed5e77677738aa97bc83d7ee71fa28433b
Ivan dos Santos Silva <ivan@pop-os.localdomain>
2022-01-05 22:29:17 -0300

    ajuste requisito 2

--- middlewares/getTalkerId.js
+++ middlewares/getTalkerId.js
@@ -3,7 +3,7 @@ const { read } = require('../helpers/readBody');
 const path = './talker.json';
 const responseError = { menssage: 'Pessoa palestrante não encontrada' };
 
-const getTalkerId = async (req, res) => {
+  const getTalkerId = async (req, res) => {
   const data = await read(path);
   const findTalker = data.find((talker) => talker.id === Number(req.params.id));
   return findTalker ? res.status(200).json(findTalker) : res.status(404).json(responseError);
